fdcb948463dab061bb387a34acaca3add10ebaab
pakkid <160797397+pakkid@users.noreply.github.com>
2024-10-22 18:21:40 +1300

    hehehehe

--- js/scrollSections.js
+++
@@ -1,9 +0,0 @@
-function scrollSection(sectionNumber) {
-    const targetSection = document.getElementById(`section${sectionNumber}`);
-    if (targetSection) {
-        window.scrollTo({
-            top: targetSection.offsetTop - 80, // Adjust for header height
-            behavior: 'smooth' // Smooth scroll
-        });
-    }
-}
